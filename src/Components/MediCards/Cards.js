0a922d98d937c943bd9a7ce910c43900f7f9c82f
divyaabelchada <belchadadivya@gmail.com>
2021-04-12 10:48:44 +0530

    some changes

--- src/Components/MediCards/Cards.js
+++ src/Components/MediCards/Cards.js
@@ -3,12 +3,12 @@ import Skeleton from "@material-ui/lab/Skeleton";
 import React from "react";
 import "./Cards.css";
 
-function Cards() {
+function Cards({ title }) {
   return (
     <div id="cards">
       <Paper>
         <Skeleton variant="rect" width={"100%"} height={200} />
-        <h3> Medicine category </h3>
+        <h3>{title} </h3>
       </Paper>
     </div>
   );
